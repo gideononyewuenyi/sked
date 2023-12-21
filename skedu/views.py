b29e98513164d90bd1665814d7d8c8fdb4d34b11
Gideon <go>
2023-12-21 21:51:44 +0100

    new

--- skedu/views.py
+++ skedu/views.py
@@ -11,14 +11,13 @@ from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from datetime import timedelta
-import re  # Added import for regular expressions
-import dateutil.parser as date_parser  # Import dateutil.parser for robust time extraction
+import re  
+import dateutil.parser as date_parser  
 import spacy
 nlp = spacy.load("en_core_web_sm")
 
-load_dotenv()  # This loads the environment variables from .env
+load_dotenv()  
 
-# Now you can use os.environ to access the variables
 GPT_API_KEY = os.getenv('GPT_API_KEY')
 
 # Utility function to process input with OpenAI
@@ -29,23 +28,23 @@ def process_input_with_openai(user_input):
         'Content-Type': 'application/json'
     }
     payload = {
-        'max_tokens': 50,  # Adjust as needed
-        'model': 'gpt-4',  # Specify the model you want to use
+        'max_tokens': 50,  
+        'model': 'gpt-4', 
         'messages': [{"role": "user", "content": user_input}]
     }
     response = requests.post('https://api.openai.com/v1/chat/completions', headers=headers, json=payload)
 
     # Extract the processed input from the response
     processed_input = response.json()["choices"][0]["message"]["content"]
-    return user_input + processed_input  # Return the processed input
+    return user_input + processed_input  
 
 # Utility function to set up Google Calendar API
 def setup_google_calendar_api():
     try:
-        # Load credentials from the token file
+        # Loading credentials from the token file
         creds = Credentials.from_authorized_user_file('token.json', ['https://www.googleapis.com/auth/calendar'])
 
-        # Create a Google Calendar API service
+        # Creating a Google Calendar API service
         service = build('calendar', 'v3', credentials=creds)
         return service
     except Exception as e:
@@ -54,34 +53,28 @@ def setup_google_calendar_api():
 
 # Function to find an existing event with the same start time
 def find_existing_event(service, start_time):
-    # Implement the logic to search for an existing event with the same start time
-    # You will need to query the Google Calendar API for events and check if any match the start time.
-    # If found, return the event ID; otherwise, return None.
-    # This function should be tailored to your specific requirements.
-    # Here's a placeholder implementation:
-    
     events = service.events().list(calendarId='primary', timeMin=start_time.isoformat(), singleEvents=True).execute()
     for event in events.get('items', []):
-        # Compare the start time of the event with the given start_time
+        # Comparing the start time of the event with the given start_time
         if event.get('start', {}).get('dateTime') == start_time.isoformat():
             return event['id']
-    return None  # Return None if no matching event is found
+    return None  # To Return None if no matching event is found
 
 # Function to extract appointment time from processed text
 def extract_appointment_time(user_input):
     try:
-        # Process user input with spaCy
+        # Processing user input with spaCy
         doc = nlp(user_input)
 
-        # Extract time entities from spaCy's parsed result
+        # Extracting time entities from spaCy's parsed result
         time_entities = [ent.text for ent in doc.ents if ent.label_ == "TIME"]
 
 
         if time_entities:
-            # Select the first time entity (you can customize this logic)
+            # Selecting the first time entity 
             time_str = time_entities[0]
 
-            # Parse the time string into a datetime object
+            # Parsing the time string into a datetime object
             appointment_time = date_parser.parse(time_str, fuzzy=True)
 
             return appointment_time
@@ -103,11 +96,11 @@ def schedule_appointment(request):
         if not user_input:
             return JsonResponse({"error": "No input provided"}, status=400)
 
-        # Extract the appointment date and time from the user input
+        # Extracting the appointment date and time from the user input
         appointment_time = extract_appointment_time(user_input)
 
         if appointment_time:
-            # Create a Google Calendar API service
+            # Creating a Google Calendar API service
             service = setup_google_calendar_api()
 
             # Define the event details
@@ -115,11 +108,11 @@ def schedule_appointment(request):
                 'summary': 'Scheduled Appointment',
                 'start': {
                     'dateTime': appointment_time.isoformat(),
-                    'timeZone': 'Africa/Lagos',  # Replace with the correct time zone
+                    'timeZone': 'Africa/Lagos',  
                 },
                 'end': {
                     'dateTime': (appointment_time + timedelta(hours=1)).isoformat(),
-                    'timeZone': 'Africa/Lagos',  # Replace with the correct time zone
+                    'timeZone': 'Africa/Lagos',  
                 },
             }
 
